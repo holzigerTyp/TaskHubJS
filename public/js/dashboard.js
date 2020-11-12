304000bfd4e582d75f251f9efa9a69946597a9ae
holzigerTyp <malte.wirkner@gmail.com>
2020-11-12 21:47:34 +0100

    Added function initTasksExceptSearch

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -120,6 +120,60 @@ function initTasksExceptFilter() {
     });
 }
 
+function initTasksExceptSearch() {
+    var search = document.getElementById("search");
+
+    $.ajax({
+        type: "GET",
+        url: window.location.protocol + '//' + window.location.host + "/api/gettasks",
+        crossDomain: false,
+        dataType: "json",
+        success: function(data) {
+            if(data.length != 0) {
+                for (var i = 0; i < data.length; i++) {
+                    var stat = "Error"
+                    var col = ""
+                    if(data[i].status == 0) {
+                        stat = "Open"
+                        col = "primary"
+                    } else if(data[i].status == 1) {
+                        stat = "In progress"
+                        col = "warning"
+                    } else if(data[i].status == 2) {
+                        stat = "Finished"
+                        col = "success"
+                    } else if(data[i].status == 3) {
+                        stat = "Information"
+                        col = "info"
+                    }
+    
+    
+                    var prio = "Error"
+                    var colPrio = ""
+                    if(data[i].priority == 0) {
+                        prio = "Low"
+                        colPrio = "secondary"
+                    } else if(data[i].priority == 1) {
+                        prio = "Normal"
+                        colPrio = "info"
+                    } else if(data[i].priority == 2) {
+                        prio = "Urgent"
+                        colPrio = "danger"
+                    }
+
+                    var doc = document.getElementById("overview")
+                    if((data[i].title + "").includes(search.value)) insertTask(doc, stat, col, prio, colPrio, data, i)
+                }
+                if(document.getElementById("overview").innerHTML === "") {
+                    insertErrorTask()
+                }
+            } else {
+                insertErrorTask()
+            }
+        }
+    });
+}
+
 function insertTask(doc, stat, col, prio, colPrio, data, i) {
     doc.innerHTML = doc.innerHTML + '<div class="col-12 col-md-6 col-lg-4 sys-col" id="' + stat.toLowerCase() + '/' + prio.toLowerCase() + '"><div class="clean-product-item"><div class="image"><a href="#"></a></div><div class="product-name"><a href="/dashboard/' + data[i].ID + '">' + data[i].title + '</a></div><div class="about"><div class="rating"><span class="badge badge-' + col + '">' + stat + '</span></div><div class="price"><span class="badge badge-primary badge-' + colPrio + '">' + prio + '</span></div></div></div></div>'
 }
